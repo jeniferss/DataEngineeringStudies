fb19dc3a4467d0460f541d4883842f51e68e3041
jeniferss <jen.s_souza@yahoo.com>
2024-11-18 10:56:10 -0300

    ✨ feat: adding cassandra connector to consumer

--- consumer.py
+++ consumer.py
@@ -9,6 +9,9 @@ from pyspark.sql.types import (
     FloatType
 )
 
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+from cassandra.query import SimpleStatement
 
 class OrderConsumer:
     def __init__(
@@ -25,7 +28,14 @@ class OrderConsumer:
         self.spark = SparkSession \
             .builder \
             .appName(self.appname) \
-            .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2") \
+            .config(
+                "spark.jars.packages", 
+                "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2,"
+                "com.datastax.spark:spark-cassandra-connector_2.12:3.0.0"
+            ) \
+            .config("spark.cassandra.connection.host", "127.0.0.1") \
+            .config("spark.cassandra.auth.username", "cassandra") \
+            .config("spark.cassandra.auth.password", "cassandra") \
             .getOrCreate()
 
         self.order_schema = StructType([
@@ -42,8 +52,53 @@ class OrderConsumer:
             ])), False),
             StructField("total", FloatType(), False)
         ])
+    
+    def setup_database(self):
+        cassandra_host = "127.0.0.1"
+        cassandra_port = 9042
+        cassandra_user = "cassandra"  
+        cassandra_password = "cassandra"  
+
+        auth_provider = PlainTextAuthProvider(cassandra_user, cassandra_password)
+        cluster = Cluster([cassandra_host], port=cassandra_port, auth_provider=auth_provider)
+        session = cluster.connect()
+
+        session.execute("""
+            CREATE KEYSPACE IF NOT EXISTS sales WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}
+        """)
+
+        session.set_keyspace("sales")
+
+        session.execute("""
+            CREATE TYPE IF NOT EXISTS client (
+                document TEXT,
+                name TEXT
+            );
+        """)
+
+        session.execute("""
+            CREATE TYPE IF NOT EXISTS item (
+                name TEXT,
+                price FLOAT,
+                quantity INT
+            );
+        """)
+
+        session.execute("""
+            CREATE TABLE IF NOT EXISTS orders (
+                id UUID PRIMARY KEY,
+                date TEXT,
+                client FROZEN<client>,
+                items LIST<FROZEN<item>>,
+                total float
+            )
+        """)
+
+        session.shutdown()
 
     def main(self) -> None:
+        self.setup_database()
+
         orders = self.spark.readStream \
             .format("kafka") \
             .option("kafka.bootstrap.servers", self.bootstrap_server) \
@@ -51,34 +106,37 @@ class OrderConsumer:
             .load()
 
         orders = orders.selectExpr("CAST(value AS STRING) AS orders")
-        orders = orders.selectExpr("orders", f"from_json(orders, '{self.order_schema.simpleString()}') AS order_data")
+        orders = orders.select(F.from_json("orders", self.order_schema).alias("order_data"))
 
-        formatted_orders = orders.select(
-            F.explode("order_data.items").alias("item"), 
-            "order_data.id", 
-            "order_data.date", 
-            "order_data.client", 
-            "order_data.total"
+        if not orders:
+            return
+
+        orders = orders.select(
+            F.col("order_data.id").alias("id"),
+            F.col("order_data.date").alias("date"),
+            F.col("order_data.client").alias("client"),
+            F.col("order_data.items").alias("items"),
+            F.col("order_data.total").alias("total")
         )
 
-        aggregated_orders = formatted_orders \
-            .groupBy("item.name") \
-            .agg(
-                F.sum("item.quantity").alias("quantity"),
-                F.round(
-                    F.sum(F.col("item.quantity") * F.col("item.price")), 
-                    2
-                ).alias("revenue")
-            ) \
-            .orderBy(F.col("revenue").desc())
+        def write_to_cassandra(batch_df, batch_id):
+            print(f'Salvando batch {batch_id}')
 
-        query = aggregated_orders.writeStream \
-            .outputMode("complete") \
-            .format("console") \
+            batch_df.write \
+                .format("org.apache.spark.sql.cassandra") \
+                .option("keyspace", "sales") \
+                .option("table", "orders") \
+                .mode("append") \
+                .save()
+        
+        # Start the streaming query and write the output to Cassandra
+        query = orders.writeStream \
+            .foreachBatch(write_to_cassandra) \
+            .outputMode("append") \
             .start()
 
+        # Await termination
         query.awaitTermination()
-
 
 try:
     print("\033[92m" + "\n-----------------  INCIANDO CONSUMIDOR -----------------\n" + "\033[0m")
